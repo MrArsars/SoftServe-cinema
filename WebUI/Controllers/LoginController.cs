d38263dbfb448fc94f72147f497a537a95ed4504
Ars <arseniu437@gmail.com>
2024-05-16 14:51:09 +0300

    added Identity register, login and logout

--- WebUI/Controllers/LoginController.cs
+++ WebUI/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using Core.Models.User;
 using Core.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WebUI.Models;
 
@@ -7,10 +9,53 @@ namespace WebUI.Controllers;
 
 public class LoginController : Controller
 {
-    
-    public LoginController()
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    public LoginController(UserManager<User> userManager, SignInManager<User> signInManager)
     {
-       
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+    
+    [HttpGet]
+    public IActionResult Index(string returnUrl = null)
+    {
+        return View(new LoginViewModel { ReturnUrl = returnUrl });
+    }
+ 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Index(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = 
+                await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+            }
+        }
+        return View(model);
+    }
+ 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
     }
 
     public IActionResult Index()
