6e59386ab88b795ef2fa38edd43aae66a6460f91
MoRTeR03 <myemailromanvrachuk03@gmail.com>
2024-05-19 17:20:00 +0300

    I added a page for buying tickets and designed a style for it, redid the entire design of the pages, redid the footer along with the login and registration page. Reworked the header along with its style.

--- Core/Models/User/RegisterViewModel.cs
+++ Core/Models/User/RegisterViewModel.cs
@@ -5,6 +5,10 @@ namespace Core.Models.User;
 public class RegisterViewModel
 {
     [Required]
+    [Display(Name = "Name")]
+    public string Name { get; set; }
+    
+    [Required]
     [Display(Name = "Email")]
     public string Email { get; set; }
  
--- WebUI/wwwroot/js/head.js
+++ WebUI/wwwroot/js/head.js
@@ -1,19 +1,27 @@
+// window.addEventListener('scroll', function() {
+//     var header = document.querySelector('header');
+//     if (window.scrollY > 0) {
+//       header.classList.add('scrolled');
+//     } else {
+//       header.classList.remove('scrolled');
+//     }
+//   });
+//   window.addEventListener('scroll', function() {
+//     var header = document.querySelector('header');
+//     header.classList.toggle('scrolled', window.scrollY > 0);
+//   });
+//   $(window).scroll(function() {
+//     if ($(this).scrollTop() > 0) {
+//       $('header').addClass('scrolled');
+//     } else {
+//       $('header').removeClass('scrolled');
+//     }
+//   });
 window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    if (window.scrollY > 0) {
-      header.classList.add('scrolled');
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { // Змінюйте значення за потреби
+        header.classList.add('scrolled');
     } else {
-      header.classList.remove('scrolled');
+        header.classList.remove('scrolled');
     }
-  });
-  window.addEventListener('scroll', function() {
-    var header = document.querySelector('header');
-    header.classList.toggle('scrolled', window.scrollY > 0);
-  });
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 0) {
-      $('header').addClass('scrolled');
-    } else {
-      $('header').removeClass('scrolled');
-    }
-  });
+});
